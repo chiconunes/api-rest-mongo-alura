f05314bebfcba6dce6355d5b390e5f7a21f45d41
chiconunes <fcn_jr@hotmail.com>
2022-04-23 21:59:08 -0300

    Aula 0202 - Express + organizando Source

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-const http = require("http")
-const port = 3000;
+ import app  from './src/app.js'
 
-const rotas = {
-    '/':'Curso de Node',
-    '/livros':'Entrei na pagina de livros',
-    '/autores':'Listagem de autores',
-    '/editora':'Pag de editora',
-    '/sobre':'Info sobre o projeto'
-}
-
-const server = http.createServer((req,res) => {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(port,()=>{
+const port = process.env.port||3000;
+ 
+app.listen(port,()=>{
     console.log(`Servidor esscutando em http://localhost:${port}`)
 })
