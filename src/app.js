d81bc8aad764d9add531e1d0685dc28be039fc7c
chiconunes <fcn_jr@hotmail.com>
2022-04-24 14:33:36 -0300

    Aula 0402 - Separando camadas Controller e Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 import chalk from "chalk";
 
@@ -13,20 +14,7 @@ const app = express();
 
 app.use(express.json())
 
-// const livros = [
-//     {id:1,"titulo":"Senhor dos Aneis"},
-//     {id:2,"titulo":"O Hobbit"}
-// ]
-
-app.get('/',(req,res)=>{
-    res.status(200).send('Curso de Node')
-})
-
-app.get('/livros',(req,res)=>{
-    livros.find((err,livros)=>{
-        res.status(200).json(livros)
-    })
-})
+routes(app)
  
 app.post('/livros',(req,res)=>{
     livros.push(req.body);
